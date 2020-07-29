f3335a750e74eb5000f39b9bee6347768a5ece41
pearmini <subairui@icloud.com>
2020-07-29 15:46:31 +0800

    feat(Index): layout

--- client/src/pages/index/index.jsx
+++ client/src/pages/index/index.jsx
@@ -1,11 +1,18 @@
 import Taro from '@tarojs/taro';
 import { View } from '@tarojs/components';
-import { connect } from '@tarojs/redux';
+import { AtButton } from 'taro-ui';
 
 import './index.scss';
 
-const Index = function() {
-  return <View>hello world</View>;
-};
+function Index() {
+  return (
+    <View>
+      <AtButton size='small'>选择背景图片</AtButton>
+      <AtButton size='small'>选择自拍</AtButton>
+      <AtButton size='small'>选择滤镜</AtButton>
+      <AtButton size='small'>开始绘制</AtButton>
+    </View>
+  );
+}
 
-export default connect(() => {})(Index);
+export default Index;
