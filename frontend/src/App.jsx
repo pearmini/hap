af61124008b82470080414d52754d40bce27faf1
MiniPear <pearminipro@gmail.com>
2023-11-28 21:36:02 +0800

    Refactor with styled-component (#1)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,74 @@
 import bear from "./assets/bear.png";
+import styled from "styled-components";
 import "./App.css";
+
+const Main = styled.main`
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+`;
+
+const Playground = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+  width: calc(100% - 20rem);
+`;
+
+const Header = styled.header`
+  background: black;
+  height: 6rem;
+  padding: 1rem 2rem;
+`;
+
+const HeaderTitle = styled.h2`
+  color: white;
+`;
+
+const Content = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Footer = styled.footer`
+  background: black;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 4rem;
+  padding: 0.8rem 0;
+`;
+
+const Sider = styled.aside`
+  position: fixed;
+  right: 0;
+  top: 0;
+  height: 100%;
+  z-index: 200;
+  width: 20rem;
+  background-color: #111;
+  padding: 1.8rem 1rem;
+`;
 
 function App() {
   return (
-    <main className="main">
-      <section className="playground">
-        <header className="header">
-          <h2 className="header-title">Hap</h2>
-        </header>
-        <div className="content">
+    <Main>
+      <Playground>
+        <Header>
+          <HeaderTitle>Hap</HeaderTitle>
+        </Header>
+        <Content>
           <img src={bear} width={300} />
-        </div>
-        <footer className="footer">Made by MiniPear</footer>
-      </section>
-      <aside className="aside">
+        </Content>
+        <Footer>Made by MiniPear</Footer>
+      </Playground>
+      <Sider>
         <span>Editor</span>
-      </aside>
-    </main>
+      </Sider>
+    </Main>
   );
 }
 
