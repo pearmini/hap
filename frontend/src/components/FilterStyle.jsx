dae5fdaa8c7550b80f745c80da94efe198d71808
MiniPear <pearminipro@gmail.com>
2023-12-06 09:03:39 +0800

    Add style filter

---
+++ frontend/src/components/FilterStyle.jsx
@@ -0,0 +1,68 @@
+import { useEffect, useRef, useState } from "react";
+import { Button, Flex, Select } from "antd";
+import styled from "styled-components";
+import { UndoOutlined } from "@ant-design/icons";
+import { algorithms, defaultIndex } from "../algorithms";
+
+const Preview = styled.div`
+  width: 300px;
+  height: 300px;
+`;
+
+const ButtonContainer = styled.div``;
+
+export function FilterStyle({ imageData, onFinish = () => {} }) {
+  const previewRef = useRef(null);
+  const clear = useRef(null);
+  const [selectedAlgorithm, setSelectedAlgorithm] = useState(
+    algorithms[defaultIndex].name
+  );
+
+  useEffect(() => {
+    if (!imageData) return;
+    renderAlgorithm(imageData, selectedAlgorithm);
+  }, [imageData]);
+
+  function renderAlgorithm(image, name) {
+    if (clear.current) clear.current();
+    const { render } = algorithms.find((d) => d.name === name);
+    const app = render(image, {});
+    const node = app.node();
+    if (!(node instanceof HTMLElement)) return;
+    const preview = previewRef.current;
+    preview.innerHTML = "";
+    preview.appendChild(node);
+    onFinish(node);
+    clear.current = () => app.dispose();
+  }
+
+  function onClick() {
+    renderAlgorithm(imageData, selectedAlgorithm);
+  }
+
+  function onSelect(value) {
+    setSelectedAlgorithm(value);
+    renderAlgorithm(imageData, value);
+  }
+
+  return (
+    <Flex vertical={true} align="center" gap="large">
+      <Preview ref={previewRef} />
+      <Flex gap="small">
+        <Select
+          defaultValue={algorithms[defaultIndex].name}
+          onChange={onSelect}
+          options={algorithms.map(({ name }) => ({ name, value: name }))}
+        />
+        <ButtonContainer>
+          <Button
+            type="primary"
+            onClick={onClick}
+            icon={<UndoOutlined />}
+            shape="circle"
+          ></Button>
+        </ButtonContainer>
+      </Flex>
+    </Flex>
+  );
+}
--- frontend/src/components/Playground.jsx
+++ frontend/src/components/Playground.jsx
@@ -1,33 +1,20 @@
 import { useEffect, useRef, useState } from "react";
-import { Button, Flex, Select } from "antd";
-import { UndoOutlined } from "@ant-design/icons";
+import { Flex } from "antd";
 import styled from "styled-components";
 import bear from "../assets/bear.png";
-import { algorithms, defaultIndex } from "../algorithms";
 import { loadImageData } from "../utils/loadImageData";
+import { FilterAlgorithm } from "./FilterAlgorithm";
+import { FilterStyle } from "./FilterStyle";
 
 const Avatar = styled.img`
-  margin-right: 4rem;
   width: 300px;
   height: 300px;
 `;
-
-const Preview = styled.div`
-  width: 300px;
-  height: 300px;
-`;
-
-const ButtonContainer = styled.div``;
 
 export function Playground() {
-  const previewRef = useRef(null);
   const imageRef = useRef(null);
-  const clear = useRef(null);
   const [src] = useState(bear);
   const [imageData, setImageData] = useState(null);
-  const [selectedAlgorithm, setSelectedAlgorithm] = useState(
-    algorithms[defaultIndex].name
-  );
 
   useEffect(() => {
     const image = new Image();
@@ -41,52 +28,18 @@ export function Playground() {
         data: loadImageData(image, width, height),
       };
       setImageData(imageData);
-      renderAlgorithm(imageData, selectedAlgorithm);
     };
   }, [src]);
 
-  function renderAlgorithm(image, name) {
-    if (clear.current) clear.current();
-    const { render } = algorithms.find((d) => d.name === name);
-    const app = render(image, {});
-    const node = app.node();
-    if (!(node instanceof HTMLElement)) return;
-    const preview = previewRef.current;
-    preview.innerHTML = "";
-    preview.appendChild(node);
-    clear.current = () => app.dispose();
-  }
-
-  function onClick() {
-    renderAlgorithm(imageData, selectedAlgorithm);
-  }
-
-  function onSelect(value) {
-    setSelectedAlgorithm(value);
-    renderAlgorithm(imageData, value);
+  function onAlgorithmFinish(node) {
+    console.log(node);
   }
 
   return (
-    <Flex justify="center">
+    <Flex justify="center" gap="2.5rem">
       <Avatar ref={imageRef} src={src} />
-      <Flex vertical={true} align="center" gap="large">
-        <Preview ref={previewRef} />
-        <Flex gap="small">
-          <Select
-            defaultValue={algorithms[defaultIndex].name}
-            onChange={onSelect}
-            options={algorithms.map(({ name }) => ({ name, value: name }))}
-          />
-          <ButtonContainer>
-            <Button
-              type="primary"
-              onClick={onClick}
-              icon={<UndoOutlined />}
-              shape="circle"
-            ></Button>
-          </ButtonContainer>
-        </Flex>
-      </Flex>
+      <FilterAlgorithm imageData={imageData} onFinish={onAlgorithmFinish} />
+      <FilterStyle imageData={imageData} onFinish={onAlgorithmFinish} />
     </Flex>
   );
 }
