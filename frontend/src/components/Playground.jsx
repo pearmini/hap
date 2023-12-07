1239f5e49f4308bf3cd63cd42ba79768d2d8ffe6
MiniPear <pearminipro@gmail.com>
2023-12-07 09:11:07 +0800

    Make filters reactive (#6)

--- frontend/src/components/Playground.jsx
+++ frontend/src/components/Playground.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import { Flex } from "antd";
 import styled from "styled-components";
 import bear from "../assets/bear.png";
-import { loadImageData } from "../utils/loadImageData";
+import { getImageData, loadImageData } from "../utils/loadImageData";
 import { FilterAlgorithm } from "./FilterAlgorithm";
 import { FilterStyle } from "./FilterStyle";
 
@@ -12,9 +12,12 @@ const Avatar = styled.img`
 `;
 
 export function Playground() {
+  const width = 300;
+  const height = 300;
   const imageRef = useRef(null);
   const [src] = useState(bear);
   const [imageData, setImageData] = useState(null);
+  const [imageData2, setImageData2] = useState(null);
 
   useEffect(() => {
     const image = new Image();
@@ -32,14 +35,24 @@ export function Playground() {
   }, [src]);
 
   function onAlgorithmFinish(node) {
-    console.log(node);
+    const { width, height } = node;
+    const imageData2 = {
+      width,
+      height,
+      data: getImageData(node.getContext("2d"), width, height),
+    };
+    setImageData2(imageData2);
   }
 
   return (
     <Flex justify="center" gap="2.5rem">
       <Avatar ref={imageRef} src={src} />
-      <FilterAlgorithm imageData={imageData} onFinish={onAlgorithmFinish} />
-      <FilterStyle imageData={imageData} onFinish={onAlgorithmFinish} />
+      <FilterAlgorithm
+        imageData={imageData}
+        onFinish={onAlgorithmFinish}
+        options={{ width, height }}
+      />
+      <FilterStyle imageData={imageData2} options={{ width, height }} />
     </Flex>
   );
 }
