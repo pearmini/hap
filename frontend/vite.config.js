5564981a9a2efff62bb428c03f653a2bc902cba7
pearmini <pearminipro@gmail.com>
2024-01-26 23:14:44 +0800

    Optimize UI and algorithm

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { merge } from "lodash-es";
+
+const baseConfig = defineConfig({
+  plugins: [react()],
+});
+
+const linkConfig = defineConfig({
+  server: {
+    watch: {
+      ignored: [`!**/node_modules/@charming-art/charming/**`],
+    },
+  },
+  optimizeDeps: {
+    exclude: [`@charming-art/charming/`],
+  },
+});
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(() => (process.env.LINK ? merge(baseConfig, linkConfig) : baseConfig));
