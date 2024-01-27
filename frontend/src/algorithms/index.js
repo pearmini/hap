3d1c4427f823d67031efdd0b68a91f91c3fb7c34
Bairui Su <pearminipro@gmail.com>
2024-01-28 01:06:06 +0800

    Add filter randomNoise (#14)

--- frontend/src/algorithms/index.js
+++ frontend/src/algorithms/index.js
@@ -1,9 +1,11 @@
 import { randomUniform } from "./randomUniform";
+import { randomNoise } from "./randomNoise";
 import { circlePack } from "./circlePack";
 
 export const algorithms = [
   { name: "Uniform Random", render: randomUniform },
   { name: "Circle Pack", render: circlePack },
+  { name: "Noise Random", render: randomNoise },
 ];
 
-export const defaultIndex = 1;
+export const defaultIndex = 0;
