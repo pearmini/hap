edc9b80b2ca5aad9973743c555be531d55270627
pearmini <subairui@icloud.com>
2025-11-15 15:07:27 -0500

    Update code

--- website/src/lib/filter.js
+++ website/src/lib/filter.js
@@ -6,40 +6,40 @@ export function FilterGL2(parent, {image, width, height}) {
 
   const vertexShader = `\
   #version 300 es
-  in vec2 a_position;
-  in vec2 a_texcoord;
-  in vec4 a_color;
-  out vec2 v_texcoord;
-  out vec4 v_color;
-
+  in vec2 aPos;
+  in vec2 aUV;
+  in vec4 aColor;
+  out vec2 vUV;
+  out vec4 vColor;
   void main() {
-      gl_Position = vec4(a_position, 0.0, 1.0);
-      v_texcoord = a_texcoord;
-      v_color = a_color;
+      gl_Position = vec4(aPos, 0.0, 1.0);
+      vUV = aUV;
+      vColor = aColor;
   }`;
 
   const fragmentShader = `\
   #version 300 es
   precision highp float;
-  in vec2 v_texcoord;
-  in vec4 v_color;
+  in vec2 vUV;
+  in vec4 vColor;
   uniform sampler2D u_texture;
   uniform vec4 u_colors[1000];
   out vec4 outColor;
 
   void main() {
-    outColor = sqrt(texture(u_texture, v_texcoord)) * v_color;
+    outColor = sqrt(texture(u_texture, vUV)) * vColor;
   }`;
 
   const gl = contextGL2({parent, width, height, vertexShader, fragmentShader});
   addTexture(gl, 0, image);
-  vertexMap(gl, ["a_position", 2, "a_texcoord", 2, "a_color", 4]);
+  vertexMap(gl, ["aPos", 2, "aUV", 2, "aColor", 4]);
+
+  const scaleX = d3.scaleLinear().domain([0, width]).range([-1, 1]);
+  const scaleY = d3.scaleLinear().domain([0, height]).range([1, -1]);
+  const scaleUVX = d3.scaleLinear().domain([-1, 1]).range([0, 1]);
+  const scaleUVY = d3.scaleLinear().domain([1, -1]).range([0, 1]);
 
   _.fillPolygons = (I, P, C) => {
-    const scaleX = d3.scaleLinear().domain([0, width]).range([-1, 1]);
-    const scaleY = d3.scaleLinear().domain([0, height]).range([1, -1]);
-    const scaleUVX = d3.scaleLinear().domain([-1, 1]).range([0, 1]);
-    const scaleUVY = d3.scaleLinear().domain([1, -1]).range([0, 1]);
     const triangles = I.map((i) => {
       const polygon = P[i];
       const {points, triangles} = d3.Delaunay.from(polygon);
