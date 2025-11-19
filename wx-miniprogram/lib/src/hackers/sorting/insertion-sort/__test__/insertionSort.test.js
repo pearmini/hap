10951c74a08229545b0101d488f1c77a6f7e9d0f
pearmini <subairui@icloud.com>
2025-11-19 10:38:20 -0500

    Refactor structure

--- wx-miniprogram/lib/src/hackers/sorting/insertion-sort/__test__/insertionSort.test.js
+++
@@ -1,8 +0,0 @@
-import test from '../../tester';
-import insertionSort from '../insertionSort';
-
-describe('insertion sort', () => {
-  it('should sort array', () => {
-    test(insertionSort);
-  });
-});
